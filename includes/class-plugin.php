83f26ba39578a3991e142f73b8469f10543c55cd
Shea Bunge <bungeshea@users.sourceforge.net>
2013-01-19 13:12:33 +1100

    Improve plugin boilerplate class

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -7,26 +7,38 @@ class Code_Snippets_Plugin {
 
 	public $version = 0.1;
 
-	function __construct() {
+	function __construct( $plugin_name = '' ) {
 
-		$this->plugin_name = sanitize_key( __CLASS__ );
-
+		$this->plugin_name = ( isset( $plugin_name ) ? $plugin_name : sanitize_key( __CLASS__ ) );
 		add_action( 'plugins_loaded', array( $this, 'setup' ) );
+
 	}
 
 	function setup() {
 		global $code_snippets;
 	}
 
-	function add_database_column( $sql ) {
+	function upgrade() {
 
-		$this->current_version = get_site_option( $this->plugin_name . '_version', $this->version );
+		$installed_version = get_site_option( $this->plugin_name . '_version', $this->version );
 
-		if ( $this->current_version >= $this->version ) {
-			add_action( 'plugins_loaded', array( $this, 'add_database_table_column' ) );
-			update_site_option( 'code_snippets_tags_version', $this->version );
+		if ( $this->version !== $installed_version ) {
+			update_site_option( $this->plugin_name . '_version', $this->version );
+			return true; // plugin was upgraded
 		}
+
+		return false; // plugin was not upgraded
+	}
+
+	function add_database_column( $name, $type ) {
+		global $wpdb, $code_snippets;
+
+		$code_snippets->create_tables();
+
+		$wpdb->query( $wpdb->prepare( "ALTER TABLE %s ADD COLUMN %s %s AFTER code", $wpdb->snippets, $name, $type ) );
+
+		if ( is_multisite() )
+			$wpdb->query( $wpdb->prepare( "ALTER TABLE %s ADD COLUMN %s %s AFTER code", $wpdb->ms_snippets, $name, $type ) );
 	}
 
 }
-
