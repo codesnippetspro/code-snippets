25965598df571a013a17baf09abf4ad7630ef6b4
Shea Bunge <info@bungeshea.com>
2013-07-15 18:02:43 +1000

    Revert "Added SVG minify Grunt task"
    This reverts commit c7dc48f30862d34c8a45f8232e78f22c4dd2143e.

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,17 +67,6 @@ module.exports = function(grunt) {
 					dest: 'assets/images/'
 				}]
 			}
-		},
-
-		svgmin: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: 'assets/images/',
-					src: '**/*',
-					dest: 'assets/images/'
-				}]
-			}
 		}
 
 	});
@@ -87,8 +76,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-svgmin');
 
 	grunt.registerTask( 'default', ['jshint', 'uglify', 'compass'] );
-	grunt.registerTask( 'compress-images', ['imagemin', 'svgmin']);
 };
