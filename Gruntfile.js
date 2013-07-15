bff8b4d1bb9332607889e679e6600c340b462846
Shea Bunge <info@bungeshea.com>
2013-07-15 21:39:26 +1000

    Use matchdep to load Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 	'use strict';
 
-	grunt.initConfig( {
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
 
 		watch: {
 
@@ -103,16 +105,6 @@ module.exports = function(grunt) {
 		}
 
 	});
-
-	grunt.loadNpmTasks('grunt-contrib-watch');
-
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.registerTask( 'default', ['jshint', 'uglify', 'compass'] );
 	grunt.registerTask( 'deploy', ['clean:deploy', 'copy:deploy'] );
