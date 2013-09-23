6cda1a3add8a15dc3c9b074cd611d8bf23fd0cb7
Shea Bunge <info@bungeshea.com>
2013-09-23 22:18:31 +1000

    Updated CodeMirror to 3.17

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -412,7 +412,7 @@ class Code_Snippets_Admin {
 			return;
 
 		/* CodeMirror package version */
-		$codemirror_version = '3.14';
+		$codemirror_version = '3.17';
 
 		/* CodeMirror base framework */
 
