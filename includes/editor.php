215710b4249a84ef8185c19d063ad46610190c94
Shea Bunge <info@bungeshea.com>
2014-08-11 12:11:28 +1000

    Add a fallback for JSON_UNESCAPED_SLASHES

--- includes/editor.php
+++ includes/editor.php
@@ -11,24 +11,27 @@ function code_snippets_get_editor_atts( $override_atts, $json_encode ) {
 	$options = $options['editor'];
 
 	$saved_atts = array(
-		'matchBrackets'  => true,
-		'lineNumbers'    => $options['line_numbers'],
-		'lineWrapping'   => $options['wrap_lines'],
-		'indentUnit'     => $options['indent_unit'],
-		'tabSize'        => $options['tab_size'],
-		'indentWithTabs' => $options['indent_with_tabs'],
-		'theme'          => $options['theme'],
-		'autoCloseBrackets'	=> $options['auto_close_brackets'],
+		'matchBrackets'     => true,
+		'lineNumbers'       => $options['line_numbers'],
+		'lineWrapping'      => $options['wrap_lines'],
+		'indentUnit'        => $options['indent_unit'],
+		'tabSize'           => $options['tab_size'],
+		'indentWithTabs'    => $options['indent_with_tabs'],
+		'theme'             => $options['theme'],
+		'autoCloseBrackets' => $options['auto_close_brackets'],
 	);
 
 	$atts = wp_parse_args( $override_atts, $saved_atts );
 	$atts = apply_filters( 'code_snippets_atts', $atts );
 
 	if ( $json_encode ) {
-		if (version_compare(phpversion(), '5.4.0', '<')) {
-    		$atts = json_encode( $atts );
-		}else{
-			$atts = json_encode( $atts, JSON_UNESCAPED_SLASHES );	
+
+		/* JSON_UNESCAPED_SLASHES was added in PHP 5.4 */
+		if ( version_compare( phpversion(), '5.4.0', '>=' ) ) {
+			$atts = json_encode( $atts, JSON_UNESCAPED_SLASHES );
+		} else {
+			/* Use a fallback for < 5.4 */
+			$atts = str_replace('\\/', '/', json_encode( $atts ) );
 		}
 	}
 
