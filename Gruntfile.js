37e891ea1a754cbad21a9a493ceda28a811e1b71
Shea Bunge <info@bungeshea.com>
2015-05-10 16:44:42 +1000

    Minify vendor CSS and JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,39 @@ module.exports = function(grunt) {
 				flatten: true,
 				src: 'css/build/*.css',
 				dest: 'css/min'
+			},
+			codemirror_themes: {
+				expand: true,
+				flatten: true,
+				src: 'vendor/codemirror/theme/*.css',
+				dest: 'css/min/cmthemes'
+			},
+			vendor: {
+				files: {
+					'css/min/codemirror.css': [
+						'vendor/codemirror/lib/codemirror.css'
+					],
+					'css/min/tagit.css': [
+						'js/vendor/jquery.tagit.css',
+						'js/vendor/tagit.ui-zendesk.css'
+					]
+				}
+			}
+		},
+
+		uglify: {
+			vendor: {
+				files: {
+					'js/min/codemirror.js': [
+						'vendor/codemirror/lib/codemirror.js',
+						'vendor/codemirror/mode/clike/clike.js',
+						'vendor/codemirror/mode/php/php.js',
+						'vendor/codemirror/addon/search/searchcursor.js',
+						'vendor/codemirror/addon/search/search.js',
+						'vendor/codemirror/addon/edit/matchbrackets.js'
+					],
+					'js/min/tag-it.js': ['js/vendor/tag-it.min.js']
+				}
 			}
 		},
 
@@ -74,18 +107,9 @@ module.exports = function(grunt) {
 						'includes/**/*',
 						'admin/**/*',
 						'languages/**/*',
-						'css/**/*',
-						'js/**/*',
-
-						// CodeMirror
-						'vendor/codemirror/lib/codemirror.css',
-						'vendor/codemirror/lib/codemirror.js',
-						'vendor/codemirror/mode/clike/clike.js',
-						'vendor/codemirror/mode/php/php.js',
-						'vendor/codemirror/theme/*.css',
-						'vendor/codemirror/addon/search/searchcursor.js',
-						'vendor/codemirror/addon/search/search.js',
-						'vendor/codemirror/addon/edit/matchbrackets.js'
+						'css/min/**/*',
+						'css/font/**/*',
+						'js/min/**/*'
 					],
 					dest: 'deploy/plugin',
 					filter: 'isFile'
@@ -171,5 +195,5 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'deploy', ['imagemin', 'clean:deploy', 'copy:plugin', 'copy:assets'] );
 	grunt.registerTask( 'release', ['default', 'deploy', 'wp_deploy'] );
 
-	grunt.registerTask( 'default', ['css', 'l18n'] );
+	grunt.registerTask( 'default', ['css', 'uglify', 'l18n'] );
 };
