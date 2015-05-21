64374f9f5d837d1974035df0a7aaca59c6300901
Shea Bunge <info@bungeshea.com>
2015-05-21 19:51:42 +1000

    Directly reference get_current_screen()

--- includes/admin-menus/class-edit-menu.php
+++ includes/admin-menus/class-edit-menu.php
@@ -94,7 +94,7 @@ class Code_Snippets_Edit_Menu extends Code_Snippets_Admin_Menu {
 			$snippet_id = save_snippet( stripslashes_deep( $_POST ) );
 
 			/* Update the shared network snippets if necessary */
-			if ( $screen->is_network && $snippet_id && $snippet_id > 0 ) {
+			if ( get_current_screen()->is_network && $snippet_id && $snippet_id > 0 ) {
 				$shared_snippets = get_site_option( 'shared_network_snippets', array() );
 
 				if ( isset( $_POST['snippet_sharing'] ) && 'on' === $_POST['snippet_sharing'] ) {
