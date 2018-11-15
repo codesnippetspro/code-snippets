bd9ae822248045bdff444e4b55725f15c792602d
Shea Bunge <bungeshea@gmail.com>
2018-11-15 22:58:42 +1100

    Don't bother checking the upload directory when importing seeing as it is not used

--- php/views/import.php
+++ php/views/import.php
@@ -13,7 +13,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $max_size_bytes = apply_filters( 'import_upload_size_limit', wp_max_upload_size() );
-$upload_dir = wp_upload_dir();
 
 ?>
 <div class="wrap">
@@ -25,78 +24,67 @@ $upload_dir = wp_upload_dir();
 
 		<p><?php
 			printf(
-				/* translators: %s: link to snippets admin menu */
+			/* translators: %s: link to snippets admin menu */
 				__( 'Afterwards, you will need to visit the <a href="%s">All Snippets</a> page to activate the imported snippets.', 'code-snippets' ),
 				code_snippets()->get_menu_url( 'manage' )
 			); ?></p>
 
-		<?php if ( ! empty( $upload_dir['error'] ) ) : ?>
 
-			<div class="error">
-				<p><?php _e( 'Before you can upload your import file, you will need to fix the following error:' ); ?></p>
+		<form enctype="multipart/form-data" id="import-upload-form" method="post" class="wp-upload-form" name="code_snippets_import">
+
+			<h2><?php _e( 'Duplicate Snippets', 'code-snippets' ); ?></h2>
+
+			<p class="description">
+				<?php esc_html_e( 'What should happen if an existing snippet is found with an identical name to an imported snippet?', 'code-snippets' ); ?>
+			</p>
+
+			<fieldset>
 				<p>
-					<strong><?php echo $upload_dir['error']; ?></strong>
-				</p>
-			</div>
-
-		<?php else : ?>
-
-			<form enctype="multipart/form-data" id="import-upload-form" method="post" class="wp-upload-form" name="code_snippets_import">
-
-				<h2><?php _e( 'Duplicate Snippets', 'code-snippets' ); ?></h2>
-
-				<p class="description">
-					<?php esc_html_e( 'What should happen if an existing snippet is found with an identical name to an imported snippet?', 'code-snippets' ); ?>
+					<label>
+						<input type="radio" name="duplicate_action" value="ignore" checked="checked">
+						<?php esc_html_e( 'Ignore any duplicate snippets: import all snippets from the file regardless and leave all existing snippets unchanged.', 'code-snippets' ); ?>
+					</label>
 				</p>
 
-				<fieldset>
-					<p>
-						<label>
-							<input type="radio" name="duplicate_action" value="ignore" checked="checked">
-							<?php esc_html_e( 'Ignore any duplicate snippets: import all snippets from the file regardless and leave all existing snippets unchanged.', 'code-snippets' ); ?>
-						</label>
-					</p>
-
-					<p>
-						<label>
-							<input type="radio" name="duplicate_action" value="replace">
-							<?php esc_html_e( 'Replace any existing snippets with a newly imported snippet of the same name.', 'code-snippets' ); ?>
-						</label>
-					</p>
-
-					<p>
-						<label>
-							<input type="radio" name="duplicate_action" value="skip">
-							<?php esc_html_e( 'Do not import any duplicate snippets; leave all existing snippets unchanged.', 'code-snippets' ); ?>
-						</label>
-					</p>
-				</fieldset>
-
-				<h2><?php _e( 'Upload Files', 'code-snippets' ); ?></h2>
-
-				<p class="description">
-					<?php _e( 'Choose one or more Code Snippets (.xml or .json) files to upload, then click "Upload files and import".', 'code-snippets' ); ?>
+				<p>
+					<label>
+						<input type="radio" name="duplicate_action" value="replace">
+						<?php esc_html_e( 'Replace any existing snippets with a newly imported snippet of the same name.', 'code-snippets' ); ?>
+					</label>
 				</p>
 
-				<fieldset>
-					<p>
-						<label for="upload"><?php esc_html_e( 'Choose files from your computer:', 'code-snippets' ); ?></label>
-						<?php printf(
-							/* translators: %s: size in bytes */
-							esc_html__( '(Maximum size: %s)', 'code-snippets' ),
-							size_format( $max_size_bytes )
-						); ?>
-						<input type="file" id="upload" name="code_snippets_import_files[]" size="25" accept="application/json,.json,text/xml" multiple="multiple">
-						<input type="hidden" name="action" value="save">
-						<input type="hidden" name="max_file_size" value="<?php echo esc_attr( $max_size_bytes ); ?>">
-					</p>
-				</fieldset>
+				<p>
+					<label>
+						<input type="radio" name="duplicate_action" value="skip">
+						<?php esc_html_e( 'Do not import any duplicate snippets; leave all existing snippets unchanged.', 'code-snippets' ); ?>
+					</label>
+				</p>
+			</fieldset>
 
-				<?php
-				do_action( 'code_snippets/admin/import_form' );
-				submit_button( __( 'Upload files and import', 'code-snippets' ) );
-				?>
-			</form>
-		<?php endif; ?>
+			<h2><?php _e( 'Upload Files', 'code-snippets' ); ?></h2>
+
+			<p class="description">
+				<?php _e( 'Choose one or more Code Snippets (.xml or .json) files to upload, then click "Upload files and import".', 'code-snippets' ); ?>
+			</p>
+
+			<fieldset>
+				<p>
+					<label for="upload"><?php esc_html_e( 'Choose files from your computer:', 'code-snippets' ); ?></label>
+					<?php printf(
+					/* translators: %s: size in bytes */
+						esc_html__( '(Maximum size: %s)', 'code-snippets' ),
+						size_format( $max_size_bytes )
+					); ?>
+					<input type="file" id="upload" name="code_snippets_import_files[]" size="25" accept="application/json,.json,text/xml" multiple="multiple">
+					<input type="hidden" name="action" value="save">
+					<input type="hidden" name="max_file_size" value="<?php echo esc_attr( $max_size_bytes ); ?>">
+				</p>
+			</fieldset>
+
+			<?php
+			do_action( 'code_snippets/admin/import_form' );
+			submit_button( __( 'Upload files and import', 'code-snippets' ) );
+			?>
+		</form>
 	</div>
 </div>
