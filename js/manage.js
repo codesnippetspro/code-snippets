52dccefa0c83ba157efc50dfe5f5061c931f4c4b
Shea Bunge <bungeshea@gmail.com>
2018-12-15 19:10:08 +1100

    Update manage.js to use ES6 variables

--- js/manage.js
+++ js/manage.js
@@ -5,14 +5,26 @@
 	const nonce = document.getElementById('code_snippets_ajax_nonce').value;
 	const network_admin = ('-network' === pagenow.substring(pagenow.length - '-network'.length));
 
+	/**
+	 * Utility function to loop through a DOM list
+	 * @param {HTMLCollectionBase} elements
+	 * @param {function} callback
+	 */
 	function foreach(elements, callback) {
 		for (let i = 0; i < elements.length; i++) {
 			callback(elements[i], i);
 		}
 	}
 
+	/**
+	 * Update the data of a given snippet using AJAX
+	 * @param {string}   field
+	 * @param {Element}  row_element
+	 * @param {object}   snippet
+	 * @param {function} [success_callback]
+	 */
 	function update_snippet(field, row_element, snippet, success_callback) {
-		var id_column = row_element.querySelector('.column-id');
+		const id_column = row_element.querySelector('.column-id');
 
 		if (! id_column || ! parseInt(id_column.textContent)) {
 			return;
@@ -22,9 +34,9 @@
 		snippet['shared_network'] = !!row_element.className.match(/\bshared-network-snippet\b/);
 		snippet['network'] = snippet['shared_network'] || network_admin;
 
-		var query_string = 'action=update_code_snippet&_ajax_nonce=' + nonce + '&field=' + field + '&snippet=' + JSON.stringify(snippet);
+		const query_string = 'action=update_code_snippet&_ajax_nonce=' + nonce + '&field=' + field + '&snippet=' + JSON.stringify(snippet);
 
-		var request = new XMLHttpRequest();
+		const request = new XMLHttpRequest();
 		request.open('POST', ajaxurl, true);
 		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
@@ -42,9 +54,12 @@
 
 	/* snippet priorities */
 
+	/**
+	 * Update the priority of a snippet
+	 */
 	function update_snippet_priority() {
-		var row = this.parentElement.parentElement;
-		var snippet = {'priority': this.value};
+		const row = this.parentElement.parentElement;
+		const snippet = {'priority': this.value};
 		update_snippet('priority', row, snippet);
 	}
 
@@ -55,22 +70,31 @@
 
 	/* activate/deactivate links */
 
-	 function update_view_count(view_count, increment) {
-		var n = parseInt(view_count.textContent.replace(/\((\d+)\)/, '$1') );
+	/**
+	 * Update the snippet count of a specific view
+	 * @param {HTMLElement} view_count
+	 * @param {boolean}     increment
+	 */
+	function update_view_count(view_count, increment) {
+		let n = parseInt(view_count.textContent.replace(/\((\d+)\)/, '$1') );
 		increment ? n++ : n--;
 		view_count.textContent = '(' + n.toString() + ')';
 	}
 
+	/**
+	 * Activate an inactive snippet, or deactivate an active snippet
+	 * @param {Event} e
+	 */
 	function toggle_snippet_active(e) {
 
-		var row = this.parentElement.parentElement; // switch < cell < row
-		var match = row.className.match(/\b(?:in)?active-snippet\b/);
+		const row = this.parentElement.parentElement; // switch < cell < row
+		const match = row.className.match(/\b(?:in)?active-snippet\b/);
 		if (! match) return;
 
 		e.preventDefault();
 
-		var activating = 'inactive-snippet' === match[0];
-		var snippet = {
+		const activating = 'inactive-snippet' === match[0];
+		const snippet = {
 			'active': activating
 		};
 
@@ -80,7 +104,7 @@
 				row.className.replace(/\binactive-snippet\b/, 'active-snippet') :
 				row.className.replace(/\bactive-snippet\b/, 'inactive-snippet');
 
-			var views = document.querySelector('.subsubsub');
+			const views = document.querySelector('.subsubsub');
 			update_view_count(views.querySelector('.active .count'), activating);
 			update_view_count(views.querySelector('.inactive .count'), activating);
 		});
