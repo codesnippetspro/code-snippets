6942f306f1e9595872f9b70792dbf1c08f837bf4
Shea Bunge <bungeshea@gmail.com>
2018-12-18 00:27:18 +1100

    Add missing CodeMirror addons for match highlighting and autoclosing brackets

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,9 @@
 import CodeMirror from 'codemirror/lib/codemirror';
 import 'codemirror/mode/php/php';
 import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
 import 'codemirror/addon/search/search';
+import 'codemirror/addon/search/match-highlighter';
 import './php-lint';
 
 window.Code_Snippets_CodeMirror = CodeMirror;
