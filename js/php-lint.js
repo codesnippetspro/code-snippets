7b681ffeda3ef3d483577aba6ddb7e034e238234
Shea Bunge <bungeshea@gmail.com>
2019-04-08 00:11:34 +1000

    Update CodeMirror PHP linter to work with WordPress

--- js/php-lint.js
+++ js/php-lint.js
@@ -3,123 +3,123 @@ Based on work distributed under the BSD 3-Clause License (https://rawgit.com/gla
 */
 
 import Parser from 'php-parser';
-import CodeMirror from 'codemirror';
-import 'codemirror/addon/lint/lint';
 
-'use strict';
+(function (CodeMirror) {
+	'use strict';
 
-class Linter {
+	class Linter {
 
-	constructor(code) {
-		this.code = code;
-		this.annotations = [];
+		constructor(code) {
+			this.code = code;
+			this.annotations = [];
 
-		this.function_names = new Set();
-		this.class_names = new Set();
-	}
+			this.function_names = new Set();
+			this.class_names = new Set();
+		}
 
-	lint() {
-		const parser = new Parser({
-			parser: {
-				suppressErrors: true
-			},
-			ast: {
-				withPositions: true
+		lint() {
+			const parser = new Parser({
+				parser: {
+					suppressErrors: true
+				},
+				ast: {
+					withPositions: true
+				}
+			});
+
+			try {
+				let ast = parser.parseEval(this.code);
+
+				if (ast.errors && ast.errors.length > 0) {
+					for (let i = 0; i < ast.errors.length; i++) {
+						this.annotate(ast.errors[i].message, ast.errors[i].loc);
+					}
+				}
+
+				this.visit(ast);
+
+			} catch (error) {
+				this.annotate(error.message, error);
 			}
-		});
+		}
 
-		try {
-			let ast = parser.parseEval(this.code);
+		/**
+		 * Visit nodes recursively
+		 * @param node
+		 */
+		visit(node) {
 
-			if (ast.errors && ast.errors.length > 0) {
-				for (let i = 0; i < ast.errors.length; i++) {
-					this.annotate(ast.errors[i].message, ast.errors[i].loc);
+			if (node.hasOwnProperty('kind')) {
+				this.validate(node);
+			}
+
+			if (node.hasOwnProperty('children')) {
+
+				for (const child of node.children) {
+					this.visit(child);
 				}
 			}
-
-			this.visit(ast);
-
-		} catch (error) {
-			this.annotate(error.message, error);
-		}
-	}
-
-	/**
-	 * Visit nodes recursively
-	 * @param node
-	 */
-	visit(node) {
-
-		if (node.hasOwnProperty('kind')) {
-			this.validate(node);
 		}
 
-		if (node.hasOwnProperty('children')) {
+		/**
+		 * Perform additional validations on nodes
+		 * @param node
+		 */
+		validate(node) {
 
-			for (const child of node.children) {
-				this.visit(child);
+			if (('function' === node.kind || 'class' === node.kind) && node.hasOwnProperty('name') && 'identifier' === node.name.kind) {
+
+				if ('function' === node.kind) {
+					if (this.function_names.has(node.name.name)) {
+						this.annotate(`Cannot redeclare function ${node.name.name}()`, node.name.loc);
+					} else {
+						this.function_names.add(node.name.name);
+					}
+				} else if ('class' === node.kind) {
+					if (this.class_names.has(node.name.name)) {
+						this.annotate(`Cannot redeclare class ${node.name.name}`, node.name.loc);
+					} else {
+						this.class_names.add(node.name.name);
+					}
+				}
 			}
 		}
-	}
 
-	/**
-	 * Perform additional validations on nodes
-	 * @param node
-	 */
-	validate(node) {
+		/**
+		 * Add a new lint annotation
+		 * @param {string} message
+		 * @param {Location|SyntaxError} position
+		 * @param {string} [severity]
+		 */
+		annotate(message, position, severity) {
+			let start, end;
 
-		if (('function' === node.kind || 'class' === node.kind) && node.hasOwnProperty('name') && 'identifier' === node.name.kind) {
+			if (position.lineNumber && position.columnNumber) {
+				start = CodeMirror.Pos(position.lineNumber - 1, position.columnNumber - 1);
+				end = CodeMirror.Pos(position.lineNumber - 1, position.columnNumber);
 
-			if ('function' === node.kind) {
-				if (this.function_names.has(node.name.name)) {
-					this.annotate(`Cannot redeclare function ${node.name.name}()`, node.name.loc);
+			} else if (position.start && position.end) {
+				if (position.end.offset < position.start.offset) {
+					end = CodeMirror.Pos(position.start.line - 1, position.start.column);
+					start = CodeMirror.Pos(position.end.line - 1, position.end.column);
 				} else {
-					this.function_names.add(node.name.name);
+					start = CodeMirror.Pos(position.start.line - 1, position.start.column);
+					end = CodeMirror.Pos(position.end.line - 1, position.end.column);
 				}
-			} else if ('class' === node.kind) {
-				if (this.class_names.has(node.name.name)) {
-					this.annotate(`Cannot redeclare class ${node.name.name}`, node.name.loc);
-				} else {
-					this.class_names.add(node.name.name);
-				}
+			}
+
+			if (start && end) {
+				severity = severity ? severity : 'error';
+				this.annotations.push({message: message, severity: severity, from: start, to: end});
 			}
 		}
 	}
 
-	/**
-	 * Add a new lint annotation
-	 * @param {string} message
-	 * @param {Location|SyntaxError} position
-	 * @param {string} [severity]
-	 */
-	annotate(message, position, severity) {
-		let start, end;
+	CodeMirror.registerHelper('lint', 'php', function (text, options) {
+		const linter = new Linter(text);
+		linter.lint();
 
-		if (position.lineNumber && position.columnNumber) {
-			start = CodeMirror.Pos(position.lineNumber - 1, position.columnNumber - 1);
-			end = CodeMirror.Pos(position.lineNumber - 1, position.columnNumber);
+		return linter.annotations;
+	});
 
-		} else if (position.start && position.end) {
-			if (position.end.offset < position.start.offset) {
-				end = CodeMirror.Pos(position.start.line - 1, position.start.column);
-				start = CodeMirror.Pos(position.end.line - 1, position.end.column);
-			} else {
-				start = CodeMirror.Pos(position.start.line - 1, position.start.column);
-				end = CodeMirror.Pos(position.end.line - 1, position.end.column);
-			}
-		}
-
-		if (start && end) {
-			severity = severity ? severity : 'error';
-			this.annotations.push({message: message, severity: severity, from: start, to: end});
-		}
-	}
-}
-
-CodeMirror.registerHelper('lint', 'php', function (text, options) {
-
-	const linter = new Linter(text);
-	linter.lint();
-
-	return linter.annotations;
-});
+})(wp.CodeMirror);
