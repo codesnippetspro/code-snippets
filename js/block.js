bbada6636c4e2c5745f8a41520f8877e785488e5
Shea Bunge <shea@sheabunge.com>
2020-09-11 20:53:00 +1000

    Add basic content block implementation

--- js/block.js
+++ js/block.js
@@ -1,26 +1,41 @@
 (function (wp) {
 
-	const {__} = wp.i18n;
-	const el = wp.element.createElement;
+	const {__, _x} = wp.i18n;
 	const {registerBlockType} = wp.blocks;
+	const {TextControl} = wp.components;
 
 	registerBlockType('code-snippets/content', {
 		title: __('Content Snippet', 'code-snippet'),
 		description: __('Insert a content snippet.', 'code-snippet'),
-		category: 'widget',
+		category: 'widgets',
 		icon: 'shortcode',
 		supports: {
-			// Removes support for an HTML mode.
 			html: false,
+			className: false,
+			customClassName: false,
+		},
+		attributes: {
+			snippet_id: {type: 'integer', default: 0},
+			network: {type: 'boolean', default: false},
+			php: {type: 'boolean', default: false},
+			format: {type: 'boolean', default: false},
+			shortcodes: {type: 'boolean', default: false}
 		},
 
-		edit: () => {
-			return <div> Hello in Editor. </div>;
-		},
+		edit: ({attributes, setAttributes}) => {
 
-		save: () => {
-			return <div> Hello in Save.</div>;
-		},
+			return (
+				<div className="wp-block-code-snippets-content components-placeholder">
+					<TextControl
+						type='number'
+						label={__('Snippet ID', 'code-snippets')}
+						value={attributes.snippet_id}
+						placeholder={__('Write shortcode here…')}
+						onChange={(val) => setAttributes({snippet_id: val})}
+					/>
+				</div>
+			);
+		}
 	});
 
 }(window.wp));
--- php/class-block-editor.php
+++ php/class-block-editor.php
@@ -15,7 +15,7 @@ class Block_Editor {
 		wp_register_script(
 			'code-snippets-content-block-editor',
 			plugins_url( 'js/min/block.js', $file ),
-			array( 'wp-blocks', 'wp-element', 'wp-i18n' ), $version
+			array( 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-components' ), $version
 		);
 		wp_set_script_translations( 'code-snippets-content-block-editor', 'code-snippets' );
 
@@ -32,10 +32,56 @@ class Block_Editor {
 		);
 
 		register_block_type( 'code-snippets/content', array(
-			'editor_script' => 'code-snippets-content-block-editor',
-			'editor_style'  => 'code-snippets-content-block-editor',
-			'style'         => 'code-snippets-content-block',
+			'editor_script'   => 'code-snippets-content-block-editor',
+			'editor_style'    => 'code-snippets-content-block-editor',
+			'style'           => 'code-snippets-content-block',
+			'render_callback' => array( $this, 'render_content' ),
 		) );
 	}
 
+	/**
+	 * Render the output of a content snippet block
+	 *
+	 * @param array  $atts    Block attributes.
+	 * @param string $content Block content (should be empty).
+	 *
+	 * @return string Block output.
+	 */
+	public function render_content( $atts, $content ) {
+
+		$atts = wp_parse_args( $atts, array(
+			'id'         => 0,
+			'network'    => false,
+			'php'        => false,
+			'format'     => false,
+			'shortcodes' => false,
+		) );
+
+		if ( ! $id = intval( $atts['snippet_id'] ) ) {
+			return '';
+		}
+
+		$snippet = get_snippet( $id, $atts['network'] ? true : false );
+		$content = $snippet->code;
+
+		if ( $atts['php'] ) {
+			ob_start();
+			eval( "?>\n\n" . $snippet->code . "\n\n<?php" );
+			$content = ob_get_clean();
+		}
+
+		if ( $atts['format'] ) {
+			$functions = [ 'wptexturize', 'convert_smilies', 'convert_chars', 'wpautop', 'capital_P_dangit' ];
+			foreach ( $functions as $function ) {
+				$content = call_user_func( $function, $content );
+			}
+		}
+
+		// evaluate shortcodes
+		if ( $atts['shortcodes'] ) {
+			$content = do_shortcode( $atts['format'] ? shortcode_unautop( $content ) : $content );
+		}
+
+		return $content;
+	}
 }
