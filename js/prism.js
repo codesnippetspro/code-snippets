acc5a9e42abde7e10abc6c2268a517a9e4dcc9d7
Shea Bunge <shea@sheabunge.com>
2020-10-02 12:20:53 +1000

    Move editor dependencies into its own file

--- js/prism.js
+++ js/prism.js
@@ -1,5 +1,12 @@
-
 import Prism from 'prismjs';
 import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/toolbar/prism-toolbar';
 import 'prismjs/plugins/show-language/prism-show-language';
+
+document.onreadystatechange = () => {
+	if ('complete' === document.readyState) {
+		Prism.highlightAll();
+	}
+};
