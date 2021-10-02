cabe1b01431c1d1f766e54f704badc4c259a1569
Shea Bunge <bungeshea@gmail.com>
2021-10-03 01:52:12 +1000

    Update tagger to v0.4.1

--- js/edit/tags.ts
+++ js/edit/tags.ts
@@ -5,9 +5,11 @@ import tagger from '@jcubic/tagger';
 	if (!tags_field) return;
 
 	tagger(tags_field, {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore completion option is circular
-		completion: {list: tags.available_tags},
+		completion: {
+			list: tags.available_tags,
+			delay: 400,
+			min_length: 2
+		},
 		allow_spaces: tags.allow_spaces,
 		allow_duplicates: false,
 		link: () => false
