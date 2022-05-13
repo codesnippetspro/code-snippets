ad738cb7592f2810ddc0620d6c5c01f4d26994b7
Shea Bunge <shea@sheabunge.com>
2022-05-14 01:58:27 +1000

    Fix plugin name tag

--- code-snippets.php
+++ code-snippets.php
@@ -14,8 +14,8 @@
  */
 
 /*
-Author:       Code Snippets Pro
-Author URI:   https://codesnippets.pro
+Plugin:       Code Snippets
+Plugin URI:   https://codesnippets.pro
 Description:  An easy, clean and simple way to run code snippets on your site. No need to edit to your theme's functions.php file again!
 Author:       Code Snippets Pro
 Author URI:   https://codesnippets.pro
