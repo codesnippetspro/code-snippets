2012c512eaa5f61a540f6e9a97d0e052bbb9b4d2
Shea Bunge <shea@sheabunge.com>
2022-05-17 15:10:37 +1000

    Add icon to Go Pro button to indicate it opens in an external tab

--- php/views/manage.php
+++ php/views/manage.php
@@ -50,12 +50,14 @@ $current_type = isset( $types[ $current_type ] ) ? $current_type : 'all';
 				);
 			}
 
-			echo esc_html( $label ), 'all' === $type_name ? '' : ' <span>' . esc_html( $type_name ) . '</span>', '</a>';
+			echo esc_html( $label ), 'all' === $type_name ? '' : ' <span class="badge">' . esc_html( $type_name ) . '</span>', '</a>';
 		}
 
 		?>
-		<a class="button button-large nav-tab-button go-pro-button" href="https://codesnippets.pro" target="_blank">
-			<?php echo wp_kses( __( 'Go <span>Pro</span>', 'code-snippets' ), [ 'span' => [] ] ); ?>
+		<a class="button button-large nav-tab-button go-pro-button" href="https://codesnippets.pro" target="_blank"
+		   title="Find more about Pro (opens in external tab)">
+			<?php echo wp_kses( __( 'Go <span class="badge">Pro</span>', 'code-snippets' ), [ 'span' => [ 'class' => 'badge' ] ] ); ?>
+			<span class="dashicons dashicons-external"></span>
 		</a>
 	</h2>
 
