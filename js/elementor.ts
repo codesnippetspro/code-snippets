ca74831df5426e5d65839c095537785b8b1704f0
Shea Bunge <shea@sheabunge.com>
2022-07-21 15:27:02 +1000

    Defer to global Prism when highlighting code in Elementor if it exists

--- js/elementor.ts
+++ js/elementor.ts
@@ -1,8 +1,10 @@
-import jQuery from 'jquery'
-
-jQuery(window).on('elementor/frontend/init', () => {
-	const { elementorFrontend, code_snippets_prism: Prism } = window
+window.addEventListener('elementor/frontend/init', () => {
+	const { elementorFrontend } = window
 
 	elementorFrontend.hooks.addAction('frontend/element_ready/code-snippets-source.default', () =>
-		Prism?.highlightAll())
+		window.Prism ?
+			Prism.highlightAll() :
+			window.code_snippets_prism ?
+				window.code_snippets_prism.highlightAll() :
+				console.error('Could not find instance of Prism for code-snippets-source block'))
 })
