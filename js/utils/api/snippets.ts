e1d7ba2b24596919b62a00365ced03d6c962d51e
Shea Bunge <shea@sheabunge.com>
2023-06-16 23:09:53 +1000

    PD-247 Create useAxios hook for attaching debug messages to axios methods.

--- js/utils/api/snippets.ts
+++ js/utils/api/snippets.ts
@@ -1,11 +1,16 @@
 import { useEffect, useMemo, useState } from 'react'
-import axios, { AxiosResponse } from 'axios'
+import { AxiosResponse, CreateAxiosDefaults } from 'axios'
 import { addQueryArgs } from '@wordpress/url'
 import { ExportSnippets } from '../../types/ExportSnippets'
 import { Snippet } from '../../types/Snippet'
 import { isNetworkAdmin } from '../general'
+import { useAxios } from './axios'
 
 const ROUTE_BASE = window.CODE_SNIPPETS?.restAPI.snippets
+
+const AXIOS_CONFIG: CreateAxiosDefaults = {
+	headers: { 'X-WP-Nonce': window.CODE_SNIPPETS?.restAPI.nonce }
+}
 
 export interface Snippets {
 	fetchAll: (network?: boolean | null) => Promise<AxiosResponse<Snippet[]>>
@@ -19,56 +24,43 @@ export interface Snippets {
 	exportCode: (snippet: Snippet) => Promise<AxiosResponse<string>>
 }
 
-export const useSnippetsAPI = (): Snippets => {
-	const axiosInstance = useMemo(() =>
-		axios.create({
-			headers: { 'X-WP-Nonce': window.CODE_SNIPPETS?.restAPI.nonce }
-		}), [])
+const buildURL = ({ id, network }: Snippet, action?: string) =>
+	addQueryArgs(
+		[ROUTE_BASE, id, action].filter(Boolean).join('/'),
+		{ network: network ? true : undefined }
+	)
 
-	const buildURL = ({ id, network }: Snippet, action?: string) =>
-		addQueryArgs([ROUTE_BASE, id, action].filter(Boolean).join('/'), network ? { network } : undefined)
+export const useSnippetsAPI = (): Snippets => {
+	const { get, post, del } = useAxios(AXIOS_CONFIG)
 
 	return useMemo((): Snippets => ({
 		fetchAll: network =>
-			axiosInstance.get<Snippet[]>(addQueryArgs(ROUTE_BASE, { network })),
+			get<Snippet[]>(addQueryArgs(ROUTE_BASE, { network })),
 
 		fetch: (snippetId, network) =>
-			axiosInstance.get<Snippet>(addQueryArgs(`${ROUTE_BASE}/${snippetId}`, { network })),
+			get<Snippet>(addQueryArgs(`${ROUTE_BASE}/${snippetId}`, { network })),
 
-		create: snippet => {
-			console.info(`Sending request to ${ROUTE_BASE}`, snippet)
-			return axiosInstance.post<Snippet>(`${ROUTE_BASE}`, snippet)
-				.then(response => {
-					console.info('Received response', response)
-					return response
-				})
-		},
+		create: snippet =>
+			post<Snippet, Snippet>(`${ROUTE_BASE}`, snippet),
 
-		update: snippet => {
-			const url = buildURL(snippet)
-			console.info(`Sending request to ${url}`, snippet)
-			return axiosInstance.post<Snippet, AxiosResponse<Snippet>, Snippet>(url, snippet)
-				.then(response => {
-					console.info('Received response', response)
-					return response
-				})
-		},
+		update: snippet =>
+			post<Snippet, Snippet>(buildURL(snippet), snippet),
 
 		delete: (snippet: Snippet) =>
-			axiosInstance.delete(buildURL(snippet)),
+			del(buildURL(snippet)),
 
 		activate: snippet =>
-			axiosInstance.post<Snippet, AxiosResponse<Snippet>, never>(buildURL(snippet, 'activate')),
+			post<Snippet, never>(buildURL(snippet, 'activate')),
 
 		deactivate: snippet =>
-			axiosInstance.post<Snippet, AxiosResponse<Snippet>, never>(buildURL(snippet, 'deactivate')),
+			post<Snippet, never>(buildURL(snippet, 'deactivate')),
 
 		export: snippet =>
-			axiosInstance.get<ExportSnippets>(buildURL(snippet, 'export')),
+			get<ExportSnippets>(buildURL(snippet, 'export')),
 
 		exportCode: snippet =>
-			axiosInstance.get<string, AxiosResponse<string>>(buildURL(snippet, 'export-code'))
-	}), [axiosInstance])
+			get<string>(buildURL(snippet, 'export-code'))
+	}), [get, post, del])
 }
 
 export const useSnippets = (): Snippet[] | undefined => {
