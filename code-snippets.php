bd154a22c8b8945cf0edc15b89d4df52ec8df080
Shea Bunge <shea@sheabunge.com>
2023-07-06 02:47:05 +1000

    Update Freemius SDK and bump version number to 2.4.2.1.

--- code-snippets.php
+++ code-snippets.php
@@ -8,11 +8,11 @@
  * License:      GPL-2.0-or-later
  * License URI:  license.txt
  * Text Domain:  code-snippets
- * Version:      3.4.2
+ * Version:      3.4.2.1
  * Requires PHP: 7.0
  * Requires at least: 5.0
  *
- * @version   3.4.2
+ * @version   3.4.2.1
  * @package   Code_Snippets
  * @author    Shea Bunge <shea@codesnippets.pro>
  * @copyright 2012-2023 Code Snippets Pro
@@ -35,7 +35,7 @@ if ( ! defined( 'CODE_SNIPPETS_FILE' ) && version_compare( phpversion(), '7.0', 
 	 *
 	 * @const string
 	 */
-	define( 'CODE_SNIPPETS_VERSION', '3.4.2' );
+	define( 'CODE_SNIPPETS_VERSION', '3.4.2.1' );
 
 	/**
 	 * The full path to the main file of this plugin.
