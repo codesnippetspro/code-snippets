e6550edfcab7478eb3cb6420c44e5188df3c74e4
Imants <imants.kokis@gmail.com>
2023-07-11 18:25:18 +0300

    PD-232 update CloudAI_REST_Controller

--- php/rest-api/class-cloud-ai-rest-controller.php
+++ php/rest-api/class-cloud-ai-rest-controller.php
@@ -24,8 +24,7 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 	 * @var string
 	 */
 	private $local_token;
-	private $fs_plugin_data;
-	private $freemius_licence;
+	private $cloud_ai_api;
 
 	/**
 	 * Class constructor.
@@ -34,11 +33,7 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 	 */
 	public function __construct() {
 		$this->local_token = get_setting( 'cloud', 'cloud_token' );
-		$fs_account_data = get_option('fs_accounts');
-		
-		// TODO: check if fs_account_data is not empty before accessing named properties
-		$this->fs_plugin_data = $fs_account_data['plugins']['code-snippets'];
-		$this->freemius_licence = $fs_account_data['all_licenses'][$this->fs_plugin_data->id][0]->secret_key;
+		$this->cloud_ai_api = new Cloud_AI();
 	}
 
 
@@ -55,7 +50,7 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 				[
 					'methods'             => WP_REST_Server::CREATABLE,
 					'callback'            => [ $this, 'cloud_ai_prompt' ],
-					'permission_callback' => [ $this, 'cloud_api_check' ],
+					'permission_callback' => [ $this, 'cloud_api_auth' ],
 					'args'                => $this->get_endpoint_args_for_item_schema( true ),
 				],
 				'schema' => [ $this, 'get_item_schema' ],
@@ -69,25 +64,10 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 				[
 					'methods'             => WP_REST_Server::CREATABLE,
 					'callback'            => [ $this, 'cloud_ai_explain' ],
-					'permission_callback' => [ $this, 'cloud_api_check' ],
+					'permission_callback' => [ $this, 'cloud_api_auth' ],
 					'args'                => $this->get_endpoint_args_for_item_schema( true ),
 				],
 				'schema' => [ $this, 'get_item_schema' ],
-			]
-		);
-
-		register_rest_route(
-			$this->namespace,
-			$route_base . '/check',
-			[
-				[
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => [ $this, 'cloud_ai_check' ],
-					// 'permission_callback' => [ $this, 'cloud_api_check' ],
-					// 'permission_callback' => '__return_true',
-					'args'                => $this->get_endpoint_args_for_item_schema( true ),
-				],
-				// 'schema' => [ $this, 'get_item_schema' ],
 			]
 		);
     }
@@ -99,12 +79,13 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 	 *
 	 * @return boolean
 	 */
-	public function cloud_api_check( $request ): bool {
+	public function cloud_api_auth( $request ): bool {
 		
         //Get Cloud Token from Authorization Header Bearer
 		$cloud_token = $request->get_header('access-control');
-        
-        if ( $cloud_token === $this->local_token ) {
+        $cs_cloud_api_nonce = $request->get_param('nonce') ?? '';
+
+        if ( $cloud_token === $this->local_token && wp_verify_nonce( $cs_cloud_api_nonce, 'cs_cloud_ai_api' )) {
             return true;
 		}
 
@@ -112,7 +93,7 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 	}
 
 	/**
-	 * Create one item from the collection
+	 * Get the response from Cloud AI API /prompt endpoint
 	 *
 	 * @param WP_REST_Request $request Full data about the request.
 	 *
@@ -120,58 +101,45 @@ class CloudAI_REST_Controller extends Snippets_REST_Controller {
 	 */
 	public function cloud_ai_prompt( $request ) {
 		
+		$prompt = $request->get_param('prompt') ?? '';
 		
-		//Construct success response
+		if ( empty( $prompt ) ) {
+			return new WP_Error( 'rest_empty_prompt', __( 'Prompt cannot be empty', 'code-snippets' ), [ 'status' => 400 ] );
+		}
+
+		$api_response = $this->cloud_ai_api->prompt($prompt);
 		$response = [
 			'status'  => 'success',
-			'message' => __( 'Snippet created', 'code-snippets' ),
+			'message' => $api_response,
 		];
 
-		//Return the response
 		return rest_ensure_response( $response );
 	}
 
 	/**
-	 * Create one item from the collection
+	 * Get the response from Cloud AI API /explain endpoint
 	 *
 	 * @param WP_REST_Request $request Full data about the request.
 	 *
 	 * @return WP_REST_Response|WP_Error
 	 */
 	public function cloud_ai_explain( $request ) {
+
+		$prompt = $request->get_param('prompt') ?? '';
 		
-		
-		//Construct success response
+		if ( empty( $prompt ) ) {
+			return new WP_Error( 'rest_empty_prompt', __( 'Prompt cannot be empty', 'code-snippets' ), [ 'status' => 400 ] );
+		}
+
+		$api_response = $this->cloud_ai_api->explain($prompt);
 		$response = [
 			'status'  => 'success',
-			'message' => __( 'Snippet created', 'code-snippets' ),
+			'message' => $api_response,
+			// 'key' => wp_create_nonce( 'cs_cloud_ai_api' ),
 		];
 
-		//Return the response
 		return rest_ensure_response( $response );
 	}
 
-	/**
-	 * Create one item from the collection
-	 *
-	 * @param WP_REST_Request $request Full data about the request.
-	 *
-	 * @return WP_REST_Response|WP_Error
-	 */
-	public function cloud_ai_check( $request ) {
-		
-		$cloud_ai = new Cloud_AI();
-		//Construct success response
-		$response = [
-			'status'  => 'success',
-			'message' => [
-				'cloud_ai' => $cloud_ai->prompt('create a snippet that will add a new admin notice to the dashboard with the message "Great stuff! Code Snippets AI is awesome!"'),
-				'local_token' => $this->local_token,
-				'freemius_licence' => $this->freemius_licence,
-			],
-		];
 
-		//Return the response
-		return rest_ensure_response( $response );
-	}
 }
