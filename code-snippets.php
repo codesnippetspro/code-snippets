7470c2dd64bf9227f684eda45997c406cce0fc6e
Shea Bunge <shea@sheabunge.com>
2023-10-18 16:23:18 +1100

    PD-232 Update changelog and version number for version 3.6.0-beta.1.

--- code-snippets.php
+++ code-snippets.php
@@ -8,11 +8,11 @@
  * License:      GPL-2.0-or-later
  * License URI:  license.txt
  * Text Domain:  code-snippets
- * Version:      3.6.0-alpha.1
+ * Version:      3.6.0-beta.1
  * Requires PHP: 7.4
  * Requires at least: 5.0
  *
- * @version   3.6.0-alpha.1
+ * @version   3.6.0-beta.1
  * @package   Code_Snippets
  * @author    Shea Bunge <shea@codesnippets.pro>
  * @copyright 2012-2023 Code Snippets Pro
@@ -37,7 +37,7 @@ if ( ! defined( 'CODE_SNIPPETS_FILE' ) && version_compare( phpversion(), '7.4', 
 	 *
 	 * @const string
 	 */
-	define( 'CODE_SNIPPETS_VERSION', '3.6.0-alpha.1' );
+	define( 'CODE_SNIPPETS_VERSION', '3.6.0-beta.1' );
 
 	/**
 	 * The full path to the main file of this plugin.
