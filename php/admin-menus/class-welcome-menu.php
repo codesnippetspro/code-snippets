16d12bb5a5571241b3961d4673b569f6a33f4dd0
Shea Bunge <shea@sheabunge.com>
2024-05-23 02:36:29 +1000

    PD-298 Hide Go Pro button on welcome menu for licensed users.

--- php/admin-menus/class-welcome-menu.php
+++ php/admin-menus/class-welcome-menu.php
@@ -213,7 +213,7 @@ class Welcome_Menu extends Admin_Menu {
 	 * @return array<string, array{url: string, icon: string, label: string}>
 	 */
 	protected function get_header_links(): array {
-		return [
+		$links = [
 			'cloud'     => [
 				'url'   => 'https://codesnippets.cloud',
 				'icon'  => 'cloud',
@@ -234,12 +234,17 @@ class Welcome_Menu extends Admin_Menu {
 				'icon'  => 'discord',
 				'label' => __( 'Discord', 'code-snippets' ),
 			],
-			'pro'       => [
+		];
+
+		if ( ! code_snippets()->licensing->is_licensed() ) {
+			$links['pro'] = [
 				'url'   => 'https://codesnippets.pro/pricing/',
 				'icon'  => 'cart',
 				'label' => __( 'Get Pro', 'code-snippets' ),
-			],
-		];
+			];
+		}
+
+		return $links;
 	}
 
 	/**
