82698f0622446903d5453a7e6319b9083680034c
Shea Bunge <shea@sheabunge.com>
2024-08-19 07:01:00 +1000

    Fix outdated PrismJS line-highlight CSS rules to avoid webpack warnings.

--- config/modules/postcss-easy-import.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'postcss-easy-import' {
-	import type { Plugin } from 'postcss'
-
-	export default function (opts: {
-		prefix: string | boolean
-		extensions: string | string[]
-	}): Plugin
-}
