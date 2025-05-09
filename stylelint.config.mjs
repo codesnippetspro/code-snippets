b603493482f69e5956f81034d9c4122530e3ee6d
Shea Bunge <shea@sheabunge.com>
2025-05-09 15:31:07 +1000

    Enforce use of logical properties through stylelint.

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('stylelint').Config} */
 export default {
 	extends: ['stylelint-config-standard-scss'],
-	plugins: ['stylelint-scss'],
+	plugins: [
+		'stylelint-scss',
+		'stylelint-use-logical'
+	],
 	customSyntax: 'postcss-scss',
 	rules: {
 		'at-rule-no-unknown': null,
+		'csstools/use-logical': 'always',
 		'font-family-no-missing-generic-family-keyword': [true, { ignoreFontFamilies: ['dashicons'] }],
 		'selector-id-pattern': null,
 		'selector-class-pattern': null,
