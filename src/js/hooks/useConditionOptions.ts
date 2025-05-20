c1e5a899cca68babbf5a5d5edcf3452c5cb7ae5a
Shea Bunge <shea@sheabunge.com>
2025-05-20 15:53:46 +1000

    Simplify functions in useConditionOptions.

--- src/js/hooks/useConditionOptions.ts
+++ src/js/hooks/useConditionOptions.ts
@@ -1,15 +1,68 @@
 import { useCallback, useEffect, useState } from 'react'
 import { CONDITION_SUBJECTS } from '../utils/conditions/subjects'
-import { handleUnknownError } from '../utils/errors'
 import { REST_API_AXIOS_CONFIG } from '../utils/restAPI'
 import { useAxios } from './useAxios'
 import { useSnippetForm } from './useSnippetForm'
-import type { ConditionSubjectDefinitions } from '../types/ConditionSubjectDefinitions'
 import type { AxiosAPI } from './useAxios'
-import type { Snippet } from '../types/Snippet'
 import type { Dispatch, SetStateAction } from 'react'
+import type { ConditionSubjectDefinitions } from '../types/ConditionSubjectDefinitions'
 import type { ConditionSubject, ConditionSubjects } from '../types/ConditionSubject'
 import type { SelectGroups } from '../types/SelectOption'
+
+type ConditionSubjectOptions = { [S in ConditionSubject]?: SelectGroups<ConditionSubjects[S]> }
+
+interface SubjectWithDefinition<S extends ConditionSubject> {
+	subject: S
+	definition: ConditionSubjectDefinitions<ConditionSubjects>[S]
+}
+
+const findSubjectDefinition = <S extends ConditionSubject>(selectedSubject: S): SubjectWithDefinition<S> => {
+	const subject = (<S | undefined> CONDITION_SUBJECTS[selectedSubject].useSubjectOptions) ?? selectedSubject
+	const definition = CONDITION_SUBJECTS[subject]
+	return { subject, definition }
+}
+
+const usePagedConditionOptions = <S extends ConditionSubject>(
+	restAPI: AxiosAPI,
+	selectedSubject: S | undefined,
+	setOptionsCache: Dispatch<SetStateAction<ConditionSubjectOptions>>,
+	handleOptionsLoaded: (setOptions: SetStateAction<SelectGroups<ConditionSubjects[S]> | undefined>) => void
+) => {
+	const [currentPage, setCurrentPage] = useState<Partial<Record<ConditionSubject, number>>>({})
+	const [loadingOptions, setLoadingOptions] = useState(false)
+
+	const loadPagedOptions = useCallback(({ subject, definition }: SubjectWithDefinition<S>) => {
+		if (definition.fetchPagedOptions && !loadingOptions) {
+			const newPage = (currentPage[subject] ?? 0) + 1
+			setLoadingOptions(true)
+
+			definition.fetchPagedOptions(restAPI, newPage)
+				.then(options => {
+					setCurrentPage(previous => ({ ...previous, [subject]: newPage }))
+					setOptionsCache(previous => ({ ...previous, [subject]: [...previous[subject] ?? [], ...options] }))
+					handleOptionsLoaded(previous => [...previous ?? [], ...options])
+					setLoadingOptions(false)
+				})
+				.catch((error: unknown) => {
+					console.error(error)
+					setCurrentPage(previous => ({ ...previous, [subject]: -1 }))
+					setLoadingOptions(false)
+				})
+		}
+	}, [restAPI, currentPage, loadingOptions, setOptionsCache, handleOptionsLoaded])
+
+	const loadMoreOptions = useCallback(() => {
+		if (selectedSubject !== undefined && !loadingOptions) {
+			const { subject, definition } = findSubjectDefinition(selectedSubject)
+
+			if (definition.fetchPagedOptions && -1 !== currentPage[subject]) {
+				loadPagedOptions({ subject, definition })
+			}
+		}
+	}, [currentPage, selectedSubject, loadingOptions, loadPagedOptions])
+
+	return { loadMoreOptions, loadPagedOptions }
+}
 
 export interface UseConditionOptions<S extends ConditionSubject> {
 	loadedSubject: ConditionSubject | undefined
@@ -18,116 +71,55 @@ export interface UseConditionOptions<S extends ConditionSubject> {
 	clearObjectOptions: VoidFunction
 }
 
-type ConditionSubjectOptions = { [S in ConditionSubject]?: SelectGroups<ConditionSubjects[S]> }
-
-const findSubjectDefinition = <S extends ConditionSubject>(currentSubject: S): {
-	subject: S
-	definition: ConditionSubjectDefinitions<ConditionSubjects>[S]
-} => {
-	const subject = (<S | undefined> CONDITION_SUBJECTS[currentSubject].useSubjectOptions) ?? currentSubject
-	const definition = CONDITION_SUBJECTS[subject]
-	return { subject, definition }
-}
-
-const resolveOptions = <S extends ConditionSubject>({
-	restAPI,
-	snippet,
-	currentPage,
-	snippetsList,
-	optionsCache,
-	currentSubject,
-	setCurrentPage
-}: {
-	restAPI: AxiosAPI,
-	snippet: Snippet,
-	currentPage: Partial<Record<ConditionSubject, number>>,
-	snippetsList: readonly Snippet[] | undefined,
-	optionsCache: ConditionSubjectOptions,
-	currentSubject: S,
-	setCurrentPage: Dispatch<SetStateAction<Partial<Record<ConditionSubject, number>>>>
-}): SelectGroups<ConditionSubjects[S]> | Promise<SelectGroups<ConditionSubjects[S]>> | undefined => {
-	const { subject, definition } = findSubjectDefinition(currentSubject)
-
-	if (optionsCache[subject]) {
-		return optionsCache[subject]
-	}
-
-	if (definition.options) {
-		return definition.options
-	}
-
-	if (definition.deriveOptions) {
-		return snippetsList && definition.deriveOptions(snippet, snippetsList)
-	}
-
-	if (definition.fetchAllOptions) {
-		return definition.fetchAllOptions(restAPI)
-	}
-
-	if (definition.fetchPagedOptions) {
-		const page = (currentPage[subject] ?? 0) + 1
-		setCurrentPage(previous => ({ ...previous, [subject]: page }))
-		return definition.fetchPagedOptions(restAPI, page)
-	}
-
-	return undefined
-}
-
-export const useConditionOptions = <S extends ConditionSubject>(currentSubject: S | undefined): UseConditionOptions<S> => {
+export const useConditionOptions = <S extends ConditionSubject>(selectedSubject: S | undefined): UseConditionOptions<S> => {
 	const restAPI = useAxios(REST_API_AXIOS_CONFIG)
 	const { snippet, snippetsList } = useSnippetForm()
-	const [currentPage, setCurrentPage] = useState<Partial<Record<ConditionSubject, number>>>({})
 	const [optionsCache, setOptionsCache] = useState<ConditionSubjectOptions>({})
 	const [loadedSubject, setLoadedSubject] = useState<ConditionSubject>()
 	const [objectOptions, setObjectOptions] = useState<SelectGroups<ConditionSubjects[S]> | undefined>(undefined)
 	const [loadingOptions, setLoadingOptions] = useState(false)
 
-	useEffect(() => {
-		if (objectOptions === undefined && currentSubject !== undefined && !loadingOptions) {
-			const resolvedOptions = resolveOptions(
-				{ currentSubject, optionsCache, snippet, snippetsList, currentPage, setCurrentPage, restAPI })
+	const handleOptionsLoaded = useCallback((options: SetStateAction<SelectGroups<ConditionSubjects[S]> | undefined>) => {
+		setLoadedSubject(selectedSubject)
+		setObjectOptions(options)
+	}, [selectedSubject])
 
-			if (resolvedOptions instanceof Promise) {
-				setLoadingOptions(true)
+	const { loadPagedOptions, loadMoreOptions } = usePagedConditionOptions(restAPI, selectedSubject, setOptionsCache, handleOptionsLoaded)
 
-				resolvedOptions.then(options => {
-					setObjectOptions(options)
-					setLoadedSubject(currentSubject)
-					setOptionsCache(previous => ({ ...previous, [currentSubject]: options }))
-				}).catch(handleUnknownError).finally(() => {
+	const loadAllOptions = useCallback(({ subject, definition }: SubjectWithDefinition<S>) => {
+		if (definition.fetchAllOptions && !loadingOptions) {
+			setLoadingOptions(true)
+
+			definition.fetchAllOptions(restAPI)
+				.then(options => {
+					setOptionsCache(previous => ({ ...previous, [subject]: options }))
+					handleOptionsLoaded(options)
 					setLoadingOptions(false)
 				})
-			} else if (resolvedOptions !== undefined) {
-				setObjectOptions(resolvedOptions)
-				setLoadedSubject(currentSubject)
-			}
-		}
-	}, [currentPage, currentSubject, loadingOptions, objectOptions, optionsCache, restAPI, snippet, snippetsList])
-
-	const loadMoreOptions = useCallback(() => {
-		if (currentSubject !== undefined && !loadingOptions) {
-			const { subject, definition } = findSubjectDefinition(currentSubject)
-
-			if (definition.fetchPagedOptions && -1 !== currentPage[subject]) {
-				const newPage = (currentPage[subject] ?? 0) + 1
-				setLoadingOptions(true)
-
-				definition.fetchPagedOptions(restAPI, newPage).then(options => {
-					setCurrentPage(previous => ({ ...previous, [subject]: newPage }))
-					setLoadingOptions(false)
-					setObjectOptions(previous => [...previous ?? [], ...options])
-					setOptionsCache(previous => ({
-						...previous,
-						[subject]: [...previous[subject] ?? [], ...options]
-					}))
-				}).catch((error: unknown) => {
+				.catch((error: unknown) => {
 					console.error(error)
-					setCurrentPage(previous => ({ ...previous, [subject]: -1 }))
 					setLoadingOptions(false)
 				})
+		}
+	}, [handleOptionsLoaded, loadingOptions, restAPI])
+
+	useEffect(() => {
+		if (objectOptions === undefined && selectedSubject !== undefined) {
+			const { subject, definition } = findSubjectDefinition(selectedSubject)
+
+			if (optionsCache[subject]) {
+				handleOptionsLoaded(optionsCache[subject])
+			} else if (definition.options) {
+				handleOptionsLoaded(definition.options)
+			} else if (definition.deriveOptions && snippetsList) {
+				handleOptionsLoaded(definition.deriveOptions(snippet, snippetsList))
+			} else if (definition.fetchAllOptions) {
+				loadAllOptions({ subject, definition })
+			} else if (definition.fetchPagedOptions) {
+				loadPagedOptions({ subject, definition })
 			}
 		}
-	}, [currentPage, currentSubject, loadingOptions, restAPI])
+	}, [handleOptionsLoaded, loadAllOptions, loadPagedOptions, objectOptions, optionsCache, selectedSubject, snippet, snippetsList])
 
 	const clearObjectOptions = useCallback(() => {
 		setLoadedSubject(undefined)
