c7bc624da6eeeb0bb20dab4a0ee5eb81acbed245
Shea Bunge <shea@sheabunge.com>
2025-06-28 02:32:57 +1000

    Update version number to 3.7.0-alpha.1.

--- src/php/views/partials/list-table-notices.php
+++ src/php/views/partials/list-table-notices.php
@@ -23,8 +23,8 @@ if ( defined( 'CODE_SNIPPETS_SAFE_MODE' ) && CODE_SNIPPETS_SAFE_MODE ) {
 			<strong><?php esc_html_e( 'Warning:', 'code-snippets' ); ?></strong>
 			<?php
 			// translators: 1: constant name, 2: file name.
-			$text = esc_html__( 'Safe mode is active and snippets will not execute! Remove the %1$s constant from %2$s file to turn off safe mode.', 'code-snippets' );
-			printf( $text, '<code>CODE_SNIPPETS_SAFE_MODE</code>', '<code>wp-config.php</code>' );
+			$text = __( 'Safe mode is active and snippets will not execute! Remove the %1$s constant from %2$s file to turn off safe mode.', 'code-snippets' );
+			printf( esc_html( $text ), '<code>CODE_SNIPPETS_SAFE_MODE</code>', '<code>wp-config.php</code>' );
 			?>
 
 			<a href="https://help.codesnippets.pro/article/12-safe-mode" target="_blank">
