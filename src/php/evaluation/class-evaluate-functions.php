d21e606805891a11f2d41df4f11bc424d289ae2d
Shea Bunge <shea@sheabunge.com>
2025-07-31 13:40:21 +1000

    Update fetch_active_snippets() function to always return a combined and sorted list.

--- src/php/evaluation/class-evaluate-functions.php
+++ src/php/evaluation/class-evaluate-functions.php
@@ -23,18 +23,6 @@ class Evaluate_Functions {
 	private DB $db;
 
 	/**
-	 * List of active snippets.
-	 *
-	 * @var array{
-	 *     id: int,
-	 *     code: string,
-	 *     scope: string,
-	 *     table: string
-	 * }
-	 */
-	private array $active_snippets = [];
-
-	/**
 	 * Class constructor.
 	 *
 	 * @param DB $db Database class instance.
@@ -45,19 +33,11 @@ class Evaluate_Functions {
 	}
 
 	/**
-	 * Fetch active snippets from the database.
+	 * Retrieve details about the currently edited snippet, if any.
 	 *
-	 * This method retrieves all active snippets from the database, excluding those that are currently being edited.
-	 * It also checks the request context to determine if the snippets should be executed.
+	 * @return ?array{id: int, table: string}
 	 */
-	private function fetch_active_snippets() {
-		$scopes = [ 'global', 'single-use', is_admin() ? 'admin' : 'front-end' ];
-		$data = $this->db->fetch_active_snippets( $scopes );
-
-		// Detect if a snippet is currently being edited, and if so, spare it from execution.
-		$edit_id = 0;
-		$edit_table = $this->db->table;
-
+	private function get_currently_editing_snippet(): ?array {
 		if ( wp_is_json_request() && ! empty( $_SERVER['REQUEST_URI'] ) ) {
 			$url = wp_parse_url( esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
 
@@ -71,33 +51,15 @@ class Evaluate_Functions {
 						? $this->db->ms_table
 						: $this->db->table;
 				}
+
+				return [
+					'id'    => $edit_id,
+					'table' => $edit_table ?? $this->db->table,
+				];
 			}
 		}
 
-		foreach ( $data as $table_name => $active_snippets ) {
-			foreach ( $active_snippets as $snippet ) {
-				$snippet_id = intval( $snippet['id'] );
-
-				if ( $edit_id !== $snippet_id || $table_name !== $edit_table ) {
-					$this->active_snippets[] = [
-						'id'       => $snippet_id,
-						'code'     => $snippet['code'],
-						'scope'    => $snippet['scope'],
-						'table'    => $table_name,
-						'priority' => intval( $snippet['priority'] ),
-					];
-				}
-			}
-		}
-
-		if ( count( $data ) > 1 ) {
-			usort(
-				$this->active_snippets,
-				function ( $snippet_a, $snippet_b ) {
-					return $snippet_a['priority'] <=> $snippet_b['priority'];
-				}
-			);
-		}
+		return null;
 	}
 
 	/**
@@ -111,47 +73,64 @@ class Evaluate_Functions {
 	}
 
 	/**
+	 * Quickly deactivate a snippet with minimal overhead.
+	 *
+	 * @param int    $snippet_id ID of the snippet to deactivate.
+	 * @param string $table_name Name of the table where the snippet is stored.
+	 *
+	 * @return void
+	 */
+	private function quick_deactivate_snippet( int $snippet_id, string $table_name ) {
+		global $wpdb;
+
+		$active_shared_ids = get_option( 'active_shared_network_snippets', [] );
+		$active_shared_ids = is_array( $active_shared_ids )
+			? array_map( 'intval', $active_shared_ids )
+			: [];
+
+		if ( $table_name === $this->db->ms_table && in_array( $snippet_id, $active_shared_ids, true ) ) {
+			unset( $active_shared_ids[ array_search( $snippet_id, $active_shared_ids, true ) ] );
+			$active_shared_ids = array_values( $active_shared_ids );
+			update_option( 'active_shared_network_snippets', $active_shared_ids );
+			clean_active_snippets_cache( $table_name );
+		} else {
+			$wpdb->update(
+				$table_name,
+				[ 'active' => '0' ],
+				[ 'id' => $snippet_id ],
+				[ '%d' ],
+				[ '%d' ]
+			);
+			clean_snippets_cache( $table_name );
+		}
+	}
+
+	/**
 	 * Evaluate applicable active snippets as early as possible.
 	 *
 	 * @return bool True if snippets were evaluated, false if safe mode is active.
 	 */
 	public function evaluate_early(): bool {
-		global $wpdb;
-
-		// Bail early if safe mode is active.
 		if ( $this->is_safe_mode_active() ) {
 			return false;
 		}
 
-		$this->fetch_active_snippets();
+		$scopes = [ 'global', 'single-use', is_admin() ? 'admin' : 'front-end' ];
+		$active_snippets = $this->db->fetch_active_snippets( $scopes );
+		$edit_snippet = $this->get_currently_editing_snippet();
 
-		foreach ( $this->active_snippets as $snippet ) {
+		foreach ( $active_snippets as $snippet ) {
 			$snippet_id = $snippet['id'];
 			$code = $snippet['code'];
 			$table_name = $snippet['table'];
 
 			// If the snippet is a single-use snippet, deactivate it before execution to ensure that the process always happens.
 			if ( 'single-use' === $snippet['scope'] ) {
-				$active_shared_ids = get_option( 'active_shared_network_snippets', array() );
-
-				if ( $table_name === $this->db->ms_table && is_array( $active_shared_ids ) && in_array( $snippet_id, $active_shared_ids, true ) ) {
-					unset( $active_shared_ids[ array_search( $snippet_id, $active_shared_ids, true ) ] );
-					$active_shared_ids = array_values( $active_shared_ids );
-					update_option( 'active_shared_network_snippets', $active_shared_ids );
-					clean_active_snippets_cache( $table_name );
-				} else {
-					$wpdb->update(
-						$table_name,
-						array( 'active' => '0' ),
-						array( 'id' => $snippet_id ),
-						array( '%d' ),
-						array( '%d' )
-					);
-					clean_snippets_cache( $table_name );
-				}
+				$this->quick_deactivate_snippet( $snippet_id, $table_name );
 			}
 
-			if ( apply_filters( 'code_snippets/allow_execute_snippet', true, $snippet_id, $table_name ) ) {
+			if ( apply_filters( 'code_snippets/allow_execute_snippet', true, $snippet_id, $table_name ) &&
+			     ( is_null( $edit_snippet ) || $edit_snippet['id'] !== $snippet_id || $edit_snippet['table'] !== $table_name ) ) {
 				execute_snippet( $code, $snippet_id );
 			}
 		}
