7d0b1ee16e2508430ff1efff4c50f6f3868999ff
Shea Bunge <shea@sheabunge.com>
2025-08-22 17:09:38 +1000

    Add missing import.

--- src/php/REST_API/REST_Endpoints.php
+++ src/php/REST_API/REST_Endpoints.php
@@ -5,6 +5,7 @@ namespace Code_Snippets\REST_API;
 use WP_REST_Request;
 use WP_REST_Response;
 use WP_REST_Server;
+use function Code_Snippets\code_snippets;
 use function Code_Snippets\Utils\delete_self_option;
 use function Code_Snippets\Utils\get_self_option;
 use const Code_Snippets\REST_API_NAMESPACE;
