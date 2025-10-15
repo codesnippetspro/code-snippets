cbf1d8fd7a086157254ff10f962fb756a483b78b
Louis Wolmarans <louiswol94@gmail.com>
2025-10-15 21:58:00 +0200

    wip: snippet title should not be a link to edit on trashed page

--- src/php/class-list-table.php
+++ src/php/class-list-table.php
@@ -364,8 +364,8 @@ class List_Table extends WP_List_Table {
 
 		$out = esc_html( $snippet->display_name );
 
-		// Add a link to the snippet if it isn't an unreadable network-only snippet.
-		if ( $this->is_network || ! $snippet->network || current_user_can( code_snippets()->get_network_cap_name() ) ) {
+		// Add a link to the snippet if it isn't an unreadable network-only snippet and isn't trashed.
+		if ( ! $snippet->is_trashed() && ( $this->is_network || ! $snippet->network || current_user_can( code_snippets()->get_network_cap_name() ) ) ) {
 			$out = sprintf(
 				'<a href="%s" class="snippet-name">%s</a>',
 				esc_attr( code_snippets()->get_snippet_edit_url( $snippet->id, $snippet->network ? 'network' : 'admin' ) ),
