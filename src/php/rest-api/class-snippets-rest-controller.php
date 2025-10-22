ff728863e39e54ad1838bc7679dec860fc7dd657
Imants <imants.kokis@gmail.com>
2025-10-22 15:39:22 +0300

    fix: improve pagination

--- src/php/rest-api/class-snippets-rest-controller.php
+++ src/php/rest-api/class-snippets-rest-controller.php
@@ -199,20 +199,15 @@ final class Snippets_REST_Controller extends WP_REST_Controller {
 		$network = $request->get_param( 'network' );
 		$all_snippets = get_snippets( [], $network );
 
-		// Collection params (page, per_page).
+		// Get collection params (page, per_page).
 		$collection_params = $this->get_collection_params();
-		$per_page = (int) $request->get_param( 'per_page' );
-		if ( ! $per_page ) {
-			$per_page = isset( $collection_params['per_page']['default'] ) ? (int) $collection_params['per_page']['default'] : 10;
-		}
-		$per_page = max( 1, $per_page );
+		$per_page_request = (int) $request->get_param( 'per_page' );
+    $per_page = max( 1, $per_page_request ? $per_page_request : (int) $collection_params['per_page']['default'] );
 
-		$page = (int) $request->get_param( 'page' );
-		if ( ! $page ) {
-			$page = isset( $collection_params['page']['default'] ) ? (int) $collection_params['page']['default'] : 1;
-		}
-		$page = max( 1, $page );
+		$page_request = (int) $request->get_param( 'page' );
+		$page = max( 1, $page_request ? $page_request : (int) $collection_params['page']['default'] );
 
+    // Count total items
 		$total_items = count( $all_snippets );
 		$total_pages = (int) ceil( $total_items / $per_page );
 
@@ -221,6 +216,7 @@ final class Snippets_REST_Controller extends WP_REST_Controller {
 		$snippets = array_slice( $all_snippets, $offset, $per_page );
 
 		$snippets_data = [];
+
 		foreach ( $snippets as $snippet ) {
 			$snippet_data = $this->prepare_item_for_response( $snippet, $request );
 			$snippets_data[] = $this->prepare_response_for_collection( $snippet_data );
