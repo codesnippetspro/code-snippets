57f0f86be08df35a0580096a5b96a17b031b6a5e
Rami Yushuvaev <ramiy@elementor.com>
2026-01-23 23:47:27 +0200

    Add missing dismiss capability

--- src/php/promotions/promotion-base.php
+++ src/php/promotions/promotion-base.php
@@ -3,9 +3,11 @@
 namespace Code_Snippets\Promotions;
 
 use function Code_Snippets\code_snippets;
-use function Code_Snippets\Settings\get_setting;
 
 abstract class Promotion_Base {
+
+	const AJAX_ACTION = 'code_snippets_dismiss_promotion';
+	const USER_META_KEY = 'code_snippets_dismissed_promotions';
 
 	abstract public function get_plugin_slug(): string;
 
@@ -35,12 +37,26 @@ abstract class Promotion_Base {
 		return $is_custom_code_page;
 	}
 
+	public function is_promotion_dismissed(): bool {
+		$user_id = get_current_user_id();
+		if ( ! $user_id ) {
+			return false;
+		}
+
+		$dismissed_promotions = get_user_meta( $user_id, self::USER_META_KEY, true );
+		if ( ! is_array( $dismissed_promotions ) ) {
+			$dismissed_promotions = [];
+		}
+
+		return in_array( $this->get_plugin_slug(), $dismissed_promotions, true );
+	}
+
 	public function display_promotion() {
 		if ( ! $this->is_plugin_admin_screen() ) {
 			return;
 		}
 
-		if ( get_setting( 'general', 'hide_promotion_' . $this->get_plugin_slug() ) ) {
+		if ( $this->is_promotion_dismissed() ) {
 			return;
 		}
 		?>
@@ -62,7 +78,10 @@ abstract class Promotion_Base {
 				margin-block-end: 0.5em;
 			}
 		</style>
-		<div class="notice notice-info is-dismissible code-snippets-promotion">
+		<div
+			class="notice notice-info is-dismissible code-snippets-promotion"
+			data-promotion-id="<?php echo esc_attr( $this->get_plugin_slug() ); ?>"
+		>
 			<div class="code-snippets-promotion-icon">
 				<img
 					src="<?php echo esc_url( plugins_url( 'assets/icon.svg', CODE_SNIPPETS_FILE ) ); ?>"
@@ -88,10 +107,53 @@ abstract class Promotion_Base {
 				</p>
 			</div>
 		</div>
+		<script>
+		jQuery(document).ready(function($) {
+			$('.code-snippets-promotion').on('click', '.notice-dismiss', function() {
+				var $notice = $(this).closest('.code-snippets-promotion');
+				var promotionId = $notice.data('promotion-id');
+
+				$.post(ajaxurl, {
+					action: '<?php echo self::AJAX_ACTION; ?>',
+					nonce: '<?php echo wp_create_nonce( self::AJAX_ACTION ); ?>',
+					promotion_id: promotionId
+				});
+			});
+		});
+		</script>
 		<?php
+	}
+
+	public function dismiss_promotion_ajax_handler() {
+		check_ajax_referer( self::AJAX_ACTION, 'nonce' );
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_send_json_error( 'Unauthorized' );
+		}
+
+		$promotion_id = isset( $_POST['promotion_id'] ) ? sanitize_text_field( $_POST['promotion_id'] ) : '';
+
+		if ( empty( $promotion_id ) ) {
+			wp_send_json_error( 'Invalid promotion id.' );
+		}
+
+		$user_id = get_current_user_id();
+		$dismissed_promotions = get_user_meta( $user_id, self::USER_META_KEY, true );
+		
+		if ( ! is_array( $dismissed_promotions ) ) {
+			$dismissed_promotions = [];
+		}
+
+		if ( ! in_array( $promotion_id, $dismissed_promotions, true ) ) {
+			$dismissed_promotions[] = $promotion_id;
+			update_user_meta( $user_id, self::USER_META_KEY, $dismissed_promotions );
+		}
+
+		wp_send_json_success();
 	}
 
 	public function __construct() {
 		add_action( 'admin_notices', [ $this, 'display_promotion' ] );
-	}    
+		add_action( 'wp_ajax_' . self::AJAX_ACTION, [ $this, 'dismiss_promotion_ajax_handler' ] );
+	}
 }
