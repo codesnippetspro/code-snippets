b63e74b02c54b8ff32608fa84f09ca98e18acd66
Rami Yushuvaev <ramiy@elementor.com>
2026-05-05 23:41:06 +0300

    Update jsx-a11y rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -139,7 +139,7 @@ export default eslintTs.config(
 			'jsx-a11y/aria-role': 'error',
 			'jsx-a11y/aria-unsupported-elements': 'error',
 			'jsx-a11y/click-events-have-key-events': 'error',
-			'jsx-a11y/control-has-associated-label': 'error',
+			'jsx-a11y/control-has-associated-label': 'warn',
 			'jsx-a11y/heading-has-content': 'error',
 			'jsx-a11y/iframe-has-title': 'error',
 			'jsx-a11y/img-redundant-alt': 'error',
@@ -149,7 +149,7 @@ export default eslintTs.config(
 			'jsx-a11y/no-noninteractive-element-interactions': 'error',
 			'jsx-a11y/no-noninteractive-tabindex': 'error',
 			'jsx-a11y/no-redundant-roles': 'error',
-			'jsx-a11y/no-static-element-interactions': 'error',
+			'jsx-a11y/no-static-element-interactions': 'warn',
 			'jsx-a11y/role-has-required-aria-props': 'error',
 			'jsx-a11y/role-supports-aria-props': 'error',
 			'jsx-a11y/tabindex-no-positive': 'error'
