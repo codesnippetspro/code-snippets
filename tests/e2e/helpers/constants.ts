e8e85b7d86488a5d9fe71513674cad52a095f692
Imants <imants.kokis@gmail.com>
2026-05-11 18:11:53 +0300

    fix: correct community cloud URL in Playwright test constants

--- tests/e2e/helpers/constants.ts
+++ tests/e2e/helpers/constants.ts
@@ -27,7 +27,7 @@ export const TIMEOUTS = <const>{
 export const URLS = <const>{
 	SNIPPETS_ADMIN: '/wp-admin/admin.php?page=snippets',
 	ADD_SNIPPET: '/wp-admin/admin.php?page=add-snippet',
-	COMMUNITY_CLOUD: '/wp-admin/admin.php?page=snippets&sub=cloud',
+	COMMUNITY_CLOUD: '/wp-admin/admin.php?page=snippets&subpage=cloud-community',
 	FRONTEND: '/'
 }
 
