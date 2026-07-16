3db3f2ab54a0ff1d88ef5ce51f934dff891983e9
Imants <imants.kokis@gmail.com>
2026-07-16 17:40:14 +0300

    feat: default to the table snippet view

--- src/js/types/SnippetView.ts
+++ src/js/types/SnippetView.ts
@@ -2,4 +2,4 @@ export const SNIPPET_VIEWS = <const> ['card', 'table']
 
 export type SnippetView = typeof SNIPPET_VIEWS[number]
 
-export const DEFAULT_SNIPPET_VIEW: SnippetView = 'card'
+export const DEFAULT_SNIPPET_VIEW: SnippetView = 'table'
