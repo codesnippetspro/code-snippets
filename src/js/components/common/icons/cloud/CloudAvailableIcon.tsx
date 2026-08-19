917e05ca68f3d21160f36a9de9eae256d57fd86b
lightbulbman <atif@lightbulb.studio>
2026-08-19 17:44:45 +0100

    feat: add cloud library styling and icons to core

---
+++ src/js/components/common/icons/cloud/CloudAvailableIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type { SVGProps } from 'react'
+
+export const CloudAvailableIcon: React.FC<SVGProps<SVGSVGElement>> = props =>
+	<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<path d="M5.09053 13.7643C3.12371 13.7643 1.5293 12.2296 1.5293 10.3362C1.5293 8.44351 3.12371 6.90874 5.09053 6.90874C5.39106 5.56133 6.46242 4.46168 7.90083 4.02351C9.33848 3.58609 10.926 3.87592 12.0639 4.78821C13.2018 5.69821 13.7172 7.08768 13.4174 8.4351H14.1745C15.6374 8.4351 16.8234 9.62804 16.8234 11.1009C16.8234 12.5745 15.6374 13.7674 14.1737 13.7674H5.09053"
+			stroke="#6A8083"
+			strokeWidth="1.41177"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
---
+++ src/js/components/common/icons/cloud/CloudErrorIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type { SVGProps } from 'react'
+
+export const CloudErrorIcon: React.FC<SVGProps<SVGSVGElement>> = props =>
+	<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<path d="M9.94107 13.7674H5.09053C3.12371 13.7643 1.5293 12.2296 1.5293 10.3362C1.5293 8.44351 3.12371 6.90874 5.09053 6.90874C5.39106 5.56133 6.46242 4.46168 7.90083 4.02351C9.33848 3.58609 10.926 3.87592 12.0639 4.78821C13.2018 5.69821 13.7172 7.08768 13.4174 8.4351H14.1745C15.2221 8.4351 16.1291 9.04686 16.5581 9.93698M16.8234 16.8232L12.9999 12.9996M12.9999 16.8232L16.8234 12.9996"
+			stroke="#DE4E7F"
+			strokeWidth="1.41177"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
