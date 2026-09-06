f86e59b5bf1b334082af77b6c27e87d4d97fb85b
Rami Yushuvaev <r_a_m_i@hotmail.com>
2026-09-06 12:01:43 +0300

    fix: render ScreenMetaSlot after Toolbar

--- src/js/components/EditMenu/EditMenu.tsx
+++ src/js/components/EditMenu/EditMenu.tsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import { ScreenMetaSlot } from '../common/ScreenMetaSlot'
 import { SnippetForm } from './SnippetForm'
 
-export const EditMenu = () =>
-	<>
-		<ScreenMetaSlot />
-		<SnippetForm />
-	</>
+export const EditMenu = () => <SnippetForm />
